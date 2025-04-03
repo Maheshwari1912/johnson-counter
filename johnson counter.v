3913b35333cdf3f0e829f3dee7763a2c8bb49811
Maheshwari1912 <mahiratthapuram@gmail.com>
2025-04-03 16:00:19 +0530

    Create johnson counter.v

---
+++ johnson counter.v
@@ -0,0 +1,14 @@
+module johnson_counter(
+  input clk,
+  input reset,
+  output reg [3:0] q);
+  
+  always@(posedge clk or posedge reset) begin
+    if(reset) begin
+      q<=4'b0000;
+    end
+    else begin
+      q<={~q[0],q[3:1]};
+    end
+  end
+endmodule
